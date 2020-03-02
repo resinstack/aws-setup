59db23b2e33590263483d95217f769d4381d879e
Michael Aldridge <aldridge.mac@gmail.com>
2020-03-01 23:26:26 -0800

    Document outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "linuxkit_role_arn" {
   value = aws_iam_role.linuxkit.arn
+  description = "ARN of the created role for use by LinuxKit during image import."
 }
 
 output "linuxkit_group_arn" {
   value = aws_iam_group.linuxkit.arn
+  description = "A group which permits the members to assume the LinuxKit role."
 }
